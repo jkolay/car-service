dfd5dd0f61519dc58b3fd510dd4d285e181aef21
jayatikolay <jayatii.kolaay@gmail.com>
2023-03-14 00:03:30 +0100

    added swagger related config

--- src/main/java/com/carlease/car/securityConfig/ProjectSecurityConfig.java
+++ src/main/java/com/carlease/car/securityConfig/ProjectSecurityConfig.java
@@ -4,7 +4,7 @@ import com.carlease.car.filter.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -69,5 +69,10 @@ public class ProjectSecurityConfig {
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return web -> web.ignoring().requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**");
   }
 }
